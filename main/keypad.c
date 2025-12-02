5bc19e86ddd5369268314ded57f00240699b3442
Rijad B <6353450@cegep-hertitage.qc.ca>
2025-12-02 10:47:27 -0500

    Updating main.c and keypad.c

--- main/keypad.c
+++ main/keypad.c
@@ -10,8 +10,8 @@ char keypad_keys[4][4] = {
     {'*','0','#','D'}
 };
 
-int row_pins[4] = {4,5,6,7};
-int col_pins[4] = {8,9,10,11};
+int row_pins[4] = {37, 36, 35, 0};
+int col_pins[4] = {45, 48, 47, 21};
 
 /*====FUNCTION DECLERATIONS====*/
 void keypad_configuration();
@@ -30,12 +30,12 @@ void keypad_configuration()
     for(int i=0;i<4;i++) //ROW PINS CONFIGURATION
     {
         gpio_set_direction(row_pins[i],GPIO_MODE_OUTPUT);
-        gpio_set_pull_mode(row_pins[i],GPIO_PULLUP_ONLY);
+        gpio_set_level(row_pins[i], 1);
     }
       for(int j=0;j<4;j++) //COLLUMN PINS CONFIGURATION
     {
         gpio_set_direction(col_pins[j],GPIO_MODE_INPUT);
-        gpio_set_level(col_pins[j], 1);
+        gpio_set_pull_mode(col_pins[j],GPIO_PULLUP_ONLY);
     }
 }
 
@@ -47,7 +47,7 @@ void keypad_configuration()
     */
 char keypad_input()
 {
-    for(int i=0;i<=4;i++)
+    for(int i=0;i<4;i++)
     {
         gpio_set_level(row_pins[i],0);
 
@@ -55,7 +55,12 @@ char keypad_input()
         {
             if(gpio_get_level(col_pins[j]) == 0)
             {
-                vTaskDelay(150/portTICK_PERIOD_MS);
+                vTaskDelay(pdMS_TO_TICKS(20)); // debounce check
+
+                // Wait until released (so no repeating)
+                while(gpio_get_level(col_pins[j]) == 0)
+                    vTaskDelay(pdMS_TO_TICKS(10));
+
                 gpio_set_level(row_pins[i], 1);
                 return keypad_keys[i][j];
             }
--- main/main.c
+++ main/main.c
@@ -30,28 +30,44 @@ void app_main(void)
         {
             printf("Enter Key Code: \n");
             asked=true;
+            printf("Key Pressed: %c",key);
         }
 
         key=keypad_input();
 
         if(key != 0)
-        {
-            printf("Key Pressed: %c",key);
-            input_buffer[key_position] = key; //The position in the array is replaced with the number we pressed
-            
+        {   
             if(key_position<4) //Limits to only 4 numbers
             {
+                input_buffer[key_position] = key; //The position in the array is replaced with the number we pressed
                 key_position++;
+
+                for(int i=0;i<4;i++)
+                {
+                    printf("%c", input_buffer[i]);
+                }
             }
+            printf("Key Pressed: %c\n", key);
+            key = 0;
         }
 
-        if(key_position == 3 && password[0] == input_buffer[0] && password[1] == input_buffer[1] && password[2] == input_buffer[2] && password[3] == input_buffer[3])
+        if(key_position == 4 && password[0] == input_buffer[0] && password[1] == input_buffer[1] && password[2] == input_buffer[2] && password[3] == input_buffer[3])
         {
             correct=true;
+            key_position=0;
         }
-        else
+        else if(key_position==4 && password[0]!=input_buffer[0] && password[1]!=input_buffer[1] && password[2]!=input_buffer[2] && password[3]!=input_buffer[3])
         {
             correct=false;
+            key_position=0;
+        }
+        if (correct==true)
+        {
+            printf("Ding");
+        }
+        else if (correct==false)
+        {
+            printf("Dong");
         }
         vTaskDelay(pdMS_TO_TICKS(10));
     }
