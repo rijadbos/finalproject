711346982b9b193f2d0e415bca88096b37555d15
Rijad B <6353450@cegep-hertitage.qc.ca>
2025-12-02 11:21:48 -0500

    modify the logic check in main.c

--- main/main.c
+++ main/main.c
@@ -15,6 +15,7 @@ char input_buffer[4];
 
 bool correct = false;
 bool asked = false;
+bool output = false;
 
 
 /*====MAIN====*/
@@ -23,14 +24,13 @@ void app_main(void)
     i2c_configuration();
     keypad_configuration();
 
-    printf("System Starting...");
     while(1)
     {
         if(asked==false) //Prints only once at the start of the while loop
         {
+            printf("System Starting...\n");
             printf("Enter Key Code: \n");
             asked=true;
-            printf("Key Pressed: %c",key);
         }
 
         key=keypad_input();
@@ -39,36 +39,40 @@ void app_main(void)
         {   
             if(key_position<4) //Limits to only 4 numbers
             {
-                input_buffer[key_position] = key; //The position in the array is replaced with the number we pressed
-                key_position++;
-
-                for(int i=0;i<4;i++)
-                {
-                    printf("%c", input_buffer[i]);
-                }
+                input_buffer[key_position]=key; //buffers the number currently pressed to an array position
+                key_position++; //updates the position of the password number to the next element for the next turn
             }
             printf("Key Pressed: %c\n", key);
             key = 0;
         }
 
-        if(key_position == 4 && password[0] == input_buffer[0] && password[1] == input_buffer[1] && password[2] == input_buffer[2] && password[3] == input_buffer[3])
+        if(key_position == 4)
         {
             correct=true;
+            for (int i=0;i<4;i++)
+            {
+                if(input_buffer[i]!=password[i])
+                {
+                    correct=false;
+                }
+            }
+            if(correct && !output)
+            {
+                output=true;
+                printf("Password:%c%c%c%c is correct\n",input_buffer[0],input_buffer[1],input_buffer[2],input_buffer[3]);
+            }
+            else if(!correct && !output)
+            {
+                output=true;
+                printf("Password:%c%c%c%c is incorrect\n",input_buffer[0],input_buffer[1],input_buffer[2],input_buffer[3]);
+            }
+
             key_position=0;
+            output=false;
+            asked=false;
+
         }
-        else if(key_position==4 && password[0]!=input_buffer[0] && password[1]!=input_buffer[1] && password[2]!=input_buffer[2] && password[3]!=input_buffer[3])
-        {
-            correct=false;
-            key_position=0;
-        }
-        if (correct==true)
-        {
-            printf("Ding");
-        }
-        else if (correct==false)
-        {
-            printf("Dong");
-        }
+
         vTaskDelay(pdMS_TO_TICKS(10));
     }
 }
